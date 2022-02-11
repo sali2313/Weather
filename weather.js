c422fd475d0c0e37f11028809c09ec5318fdc835
ali salehi <sali.up44@gmail.com>
2022-02-11 21:40:35 +0330

    second commit

--- weather.js
+++ weather.js
@@ -5,8 +5,6 @@ let temp = document.querySelector('.citytemp');
 let describe = document.querySelector('.citydes');
 
 
-
-
 $(document).ready(function(){
 
     $('#form').submit((e)=> {
@@ -23,7 +21,7 @@ $(document).ready(function(){
         
         $.get("worldcities.csv",(data)=>{
             let info = $.csv.toArrays(data);
-            // console.log(info[180][1])
+             console.log(info[180][1])
             // console.log(info)
             $.each(info,function(i){
                 if(finalname === (info[i][1]).toLowerCase()){
@@ -32,22 +30,28 @@ $(document).ready(function(){
                     let cityname = info[i][1];
                     name.innerHTML = `<a href="https://www.google.com/search?q=${cityname}">${cityname}</a>`;
                     console.log(`X = ${x}, Y = ${y}`);
-                    let apikey2 = '1e3101e3cc03399db4a9c7d1043fd62f'
-                    let secondAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${x}&lon=${y}&appid=${apikey2}`;
-                        // console.log(secondAPI)
-                    fetch(secondAPI)
+                    let apikey = '1e3101e3cc03399db4a9c7d1043fd62f'
+                    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${x}&lon=${y}&appid=${apikey}`;
+                    fetch(api)
                         .then(response=> response.json())
                             .then(jesresponse => {
                                 let finaltemp = jesresponse.main.temp;
                                 finaltemp = finaltemp - 273.15;
                                 temp.innerHTML = finaltemp.toFixed(2) + " &#8451;";
 
-                                let finaldesc = jesresponse.weather[0].description;
-                                // console.log(finaldesc)
+                                let finaldesc = jesresponse.weather[0].main;
                                 describe.innerHTML = finaldesc;
+
+                                if (finaldesc === 'Clouds'){
+                                    $('body').css('background-image', 'url("rain.jpg")')
+                                    }
+                                // console.log(finaldesc)
+                                // console.log(jesresponse.weather)
+                            //  $('p#paragraph1').html(jesresponse.weather);
                         })
                     $('p#paragraph1').text('Successed');
                     console.log("sucsess");
+                    console.log(api);
                     return false;
                 }
                 
